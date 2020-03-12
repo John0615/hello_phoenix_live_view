15f0449cd93400758b26594e247c7a99ea980cb7
john.zhao <john.zhao@newType.io>
2020-03-12 21:52:16 +0800

    移动模板至index.html.leex

---
+++ lib/hello_phoenix_live_view_web/live/live_form.ex
@@ -0,0 +1,37 @@
+defmodule HelloPhoenixLiveViewWeb.LiveForm do
+  use Phoenix.LiveView
+  use Phoenix.HTML
+  alias HelloPhoenixLiveViewWeb.LiveFormView
+
+  def mount(params, _session, socket) do
+    IO.inspect(params, label: "params>>>>> ", pretty: true)
+    socket =
+      socket
+      |> assign(:count, 0)
+      |> assign(:username_validation, true)
+    {:ok, socket}
+  end
+
+
+  def render(assigns) do
+    LiveFormView.render("index.html", assigns)
+  end
+
+  def handle_event("increment", _, socket) do
+    count = socket.assigns.count + 1
+    socket = assign(socket, :count, count)
+    { :noreply, socket }
+  end
+
+  def handle_event("decrement", _, socket) do
+    count = socket.assigns.count - 1
+    socket = assign(socket, :count, count)
+    { :noreply, socket }
+  end
+
+  def handle_event("save", _, socket) do
+    # IO.inspect(LiveViewForm, label: "params>>>>>> ", pretty: true)
+    socket = assign(socket, :username_validation, false)
+    { :noreply, socket }
+  end
+end
---
+++ lib/hello_phoenix_live_view_web/views/live_form_view.ex
@@ -0,0 +1,3 @@
+defmodule HelloPhoenixLiveViewWeb.LiveFormView do
+  use HelloPhoenixLiveViewWeb, :view
+end
