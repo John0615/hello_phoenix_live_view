8c8ac6446f350b24d4130d4166f50471541adba8
john.zhao <john.zhao@newType.io>
2020-03-26 07:51:48 +0800

    错误提示

--- lib/hello_phoenix_live_view/accounts/user.ex
+++ lib/hello_phoenix_live_view/accounts/user.ex
@@ -15,6 +15,9 @@ defmodule HelloPhoenixLiveView.Accounts.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:name, :phone, :email, :password])
-    |> validate_required([:name, :phone, :email, :password])
+    |> validate_required(:name, message: "用户名不能为空！" )
+    |> validate_required(:phone, message: "电话不能为空" )
+    |> validate_required(:email, message: "邮箱不能为空" )
+    |> validate_required(:password, message: "密码不能为空！" )
   end
 end
