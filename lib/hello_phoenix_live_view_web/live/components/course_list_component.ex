309332a693d4e76c657d0c0e282d7841207b3fd9
john.zhao <john.zhao@newType.io>
2020-04-08 22:53:51 +0800

    删除多余的html

--- lib/hello_phoenix_live_view_web/live/components/course_list_component.ex
+++ lib/hello_phoenix_live_view_web/live/components/course_list_component.ex
@@ -90,14 +90,6 @@ defmodule CourseListComponent do
       <div class="row">
         <div class="col-md-12">
           <div class="card card-primary">
-            <div class="card-header">
-              <h3 class="card-title">General</h3>
-
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-                  <i class="fas fa-minus"></i></button>
-              </div>
-            </div>
             <div class="card-body">
               <div class="form-group">
                 <label for="inputName">Project Name</label>
