eb0f331e8b44879ca28aac352c4dce1151a3b1d2
john.zhao <john.zhao@newType.io>
2020-04-30 08:19:40 +0800

    富文本编辑器

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,9 @@
 // its own CSS file.
 import css from "../css/app.css"
 import "admin-lte/dist/css/adminlte.min.css"
-// import E from 'wangeditor'
+import { wang_edit_hook } from './wang_edit_hook'
 let Hooks = {}
-Hooks.InfiniteScroll = {
-  mounted(){
-    alert()
-  }
-}
+Object.assign(Hooks, { InfiniteScroll: wang_edit_hook })
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
---
+++ assets/js/wang_edit_hook.js
@@ -0,0 +1,43 @@
+import E from 'wangeditor'
+
+function init_edit() {
+  // var E = window.wangEditor
+  var editor = new E('#editor')
+  // 编辑器的事件，每次改变会获取其html内容
+  editor.customConfig.onchange = html => {
+    alert()
+    // this.editorContent = html;
+    // this.catchData(this.editorContent); // 把这个html通过catchData的方法传入父组件
+  };
+  editor.customConfig.menus = [
+    // 菜单配置
+    'head', // 标题
+    'bold', // 粗体
+    'fontSize', // 字号
+    'fontName', // 字体
+    'italic', // 斜体
+    'underline', // 下划线
+    'strikeThrough', // 删除线
+    'foreColor', // 文字颜色
+    'backColor', // 背景颜色
+    'link', // 插入链接
+    'list', // 列表
+    'justify', // 对齐方式
+    'quote', // 引用
+    'emoticon', // 表情
+    'image', // 插入图片
+    'table', // 表格
+    'code', // 插入代码
+    'undo', // 撤销
+    'redo' // 重复
+  ];
+  editor.customConfig.uploadImgServer = '/upload'
+  editor.create()
+}
+
+let wang_edit_hook = {
+  mounted() {
+    init_edit()
+  }
+}
+export { wang_edit_hook }
